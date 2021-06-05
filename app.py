fc619021f70ac3ae5d0ed6a2a219b1b498533b30
Aravind MJ <aravind.mj@sap.com>
2021-06-05 10:52:42 +0530

    added matplotlib

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib as plt
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.write("""
